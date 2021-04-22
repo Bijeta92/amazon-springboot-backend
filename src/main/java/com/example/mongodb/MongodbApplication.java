30104a975037bddb4fb6fa65b1e4cb583de01a28
Bijeta Biswal <bijeta.biswal@gmail.com>
2021-04-22 08:33:39 -0400

    Fixed "Whitelabel Error Page" error as per https://stackoverflow.com/questions/53264877/i-have-created-spring-boot-app-and-deployed-to-heroku-but-its-not-working

--- src/main/java/com/example/mongodb/MongodbApplication.java
+++ src/main/java/com/example/mongodb/MongodbApplication.java
@@ -2,9 +2,10 @@ package com.example.mongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MongodbApplication {
+public class MongodbApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongodbApplication.class, args);
